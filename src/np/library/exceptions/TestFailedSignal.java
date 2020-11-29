aa89651e30b7b719a02f05070209620c415b0511
George Venn (Npex42) <npex42@gmail.com>
2020-11-29 13:54:10 +0000

    Updated Tester.java

--- src/np/library/exceptions/TestFailedSignal.java
+++ src/np/library/exceptions/TestFailedSignal.java
@@ -1,9 +1,36 @@
 package np.library.exceptions;
 
+import java.lang.reflect.Method;
+
 public class TestFailedSignal extends RuntimeException {
 	private static final long serialVersionUID = 1L;
+	private String method;
+	private int lineNum = 0;
+	private String causer;
 
 	public TestFailedSignal(String message) {
 		super(message);
+		super.fillInStackTrace();
+		this.method = getStackTrace()[1].getMethodName();
+		this.lineNum = getStackTrace()[1].getLineNumber();
+		this.causer = getStackTrace()[1].getClassName();
 	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public String getCauser() {
+		return causer;
+	}
+	
+	public int GetLineNum() {
+		return lineNum;
+	}
+	
+	public String GetMessage() {
+		return "["+causer+"."+method+":"+lineNum+"]: "+super.getMessage();
+	}
+	
+	
 }
